6dd085936440f042d3a462cc3520fb5b575d6a6e
chujacky <jacky9407@gmail.com>
2020-08-23 19:17:33 +0800

    Update homepage layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,51 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        hkwallmap
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <section class="hero">
+    <div class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <marker-item></marker-item>
+          </div>
+          <div class="column map-container">
+            <gmap></gmap>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import { mapMutations } from 'vuex'
+import MarkerItem from '../components/MarkerItem';
+import Gmap from '../components/Map';
+
+export default {
+  components: {
+    MarkerItem, Gmap
+  },
+}
 </script>
 
 <style>
+* {
+  font-family: 'Helvetica Neue';
+}
+
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  text-align: left;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.columns {
+  width: 100%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.map-container {
+  min-width: 70%;
 }
 
 .links {
