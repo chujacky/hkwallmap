ae163d83bd37f51be1624730780d36c2e7984f39
chujacky <jacky9407@gmail.com>
2020-08-26 21:41:02 +0800

    Add nav burger menu

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,11 +2,17 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <nuxt-link to="/" class="navbar-item">
-        <h2 class="title">hkwallmap</h2>
+        <h2 class="title">hklaxwalls</h2>
       </nuxt-link>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="toggleMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-menu">
       <div class="navbar-start">
         <nuxt-link to="/" class="navbar-item">
           Walls
@@ -21,6 +27,12 @@
 
 <script>
 export default {
-  name: 'nav-bar'
+  name: 'nav-bar',
+  methods: {
+    toggleMenu() {
+      this.$el.querySelector('.navbar-burger').classList.toggle('is-active');
+      this.$el.querySelector('.navbar-menu').classList.toggle('is-active');
+    }
+  }
 }
 </script>
