8cc94e21d055cda5488b904f5721592c8101e85c
chujacky <jacky9407@gmail.com>
2020-08-29 01:08:49 +0800

    Add lax icon

--- components/Map.vue
+++ components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <Gmap-map :center="center" :zoom="zoom" :options="mapOptions" style="width: 100%; height: calc(100vh - 52px)" @click="addMarker">
+  <Gmap-map :center="center" :zoom="zoom" :options="mapOptions" style="width: 100%; height: 100%" @click="addMarker">
     <!-- <gmap-info-window :options="infoOptions" :position="infoWindowPos" 
       :opened="infoWinOpen" @closeclick="infoWinOpen=false" style="padding: 10px;">
     </gmap-info-window> -->
@@ -9,6 +9,7 @@
       v-for="(item, index) in markers"
       :key="index"
       :position="item.position"
+      :icon="icon"
       :clickable="true"
       @click="toggleInfoWindow(item,index)"
     />
@@ -23,6 +24,10 @@
 </template>
 
 <script>
+import styles from '~/static/mapStyles.json'
+
+
+
 export default {
   name: 'gmap',
   data() {
@@ -36,219 +41,14 @@ export default {
           height: -35
         },
       },
+      icon: {
+        url: '/p-08.png',
+        size: {width: 23, height: 23, f: 'px', b: 'px'},
+        scaledSize: {width: 23, height: 23, f: 'px', b: 'px'}
+      },
       mapOptions: {
         streetViewControl: false,
-        styles: [
-    {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#444444"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            },
-            {
-                "saturation": "-65"
-            },
-            {
-                "lightness": "45"
-            },
-            {
-                "gamma": "1.78"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "saturation": "-33"
-            },
-            {
-                "lightness": "22"
-            },
-            {
-                "gamma": "2.08"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.airport",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "gamma": "2.08"
-            },
-            {
-                "hue": "#ffa200"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.airport",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.rail",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.rail",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            },
-            {
-                "saturation": "-55"
-            },
-            {
-                "lightness": "-2"
-            },
-            {
-                "gamma": "1.88"
-            },
-            {
-                "hue": "#ffab00"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#bbd9e5"
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    }]
+        styles: styles.muted
       },
     };
   },
