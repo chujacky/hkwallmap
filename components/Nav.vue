0cd014702db0689a08c09c07afb728c74d1f8652
chujacky <jacky9407@gmail.com>
2020-08-28 18:39:28 +0800

    Add favicon and nav logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,7 +2,8 @@
   <nav class="navbar is-light" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <nuxt-link to="/" class="navbar-item">
-        <h2 class="title">hklaxwalls</h2>
+        <!-- <h2 class="title">hklaxwalls</h2> -->
+        <img src="/hklaxwall-02.png" alt="">
       </nuxt-link>
 
       <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="toggleMenu">
