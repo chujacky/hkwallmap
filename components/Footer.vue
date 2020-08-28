1b433c53c48a34ebfc6e203139c51e9daf7c478a
chujacky <jacky9407@gmail.com>
2020-08-28 18:39:56 +0800

    Quick fix footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,13 @@
 <template>
   <footer class="footer">
-    <div class="level">
-      <div class="level-left">
-        <div class="level-item">
-          <div class="content">
-            <p><a href="https://www.instagram.com/hk_lax_walls/" class="social-icon" target="_blank" rel="noopener noreferrer">
-              <font-awesome-icon :icon="['fab', 'instagram']"/></a>
-              HKLAXWALLS © 2020 
-            </p>
-            <p>Website by <a href="https://github.com/chujacky/" target="_blank" rel="noopener noreferrer">Jacky Chu</a></p>
-          </div>
-        </div>  
-      </div>
-      <div class="level-right">
-        <div class="level-item">
-          <div class="content">
-          </div>
-        </div>
-      </div>
+     <div class="content">
+      <p>
+        <a href="https://www.instagram.com/hk_lax_walls/" class="social-icon" target="_blank" rel="noopener noreferrer">
+          <font-awesome-icon :icon="['fab', 'instagram']"/>
+        </a>
+      </p>
+      <p>HKLAXWALLS © 2020</p>
+      <p>Website by <a href="https://github.com/chujacky/" target="_blank" rel="noopener noreferrer">Jacky Chu</a></p>
     </div>
   </footer>
 </template>
