5ab31a907ef304b5d911f9521447f1466156ca10
chujacky <jacky9407@gmail.com>
2020-08-29 23:28:56 +0800

    Quick fix nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,7 +2,6 @@
   <nav class="navbar is-light" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <nuxt-link to="/" class="navbar-item">
-        <!-- <h2 class="title">hklaxwalls</h2> -->
         <img src="/hklaxwall-02.png" alt="">
       </nuxt-link>
 
@@ -25,7 +24,7 @@
 
       <div class="navbar-end">
         <div class="navbar-item">
-          <a href="https://www.instagram.com/hk_lax_walls/" class="link" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/hk_lax_walls/" class="social-icon" target="_blank" rel="noopener noreferrer">
             <font-awesome-icon :icon="['fab', 'instagram']"/>
           </a>
         </div>
