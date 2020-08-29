13a9004213e57addabc948eb6cca06d81fba471a
chujacky <jacky9407@gmail.com>
2020-08-30 00:39:19 +0800

    Make footer fit on screen

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,12 @@
 <template>
   <footer class="footer">
-     <div class="content">
-      <p>
-        <a href="https://www.instagram.com/hk_lax_walls/" class="social-icon" target="_blank" rel="noopener noreferrer">
-          <font-awesome-icon :icon="['fab', 'instagram']"/>
-        </a>
-      </p>
-      <p>HKLAXWALLS © 2020</p>
-      <p>Website by <a href="https://github.com/chujacky/" target="_blank" rel="noopener noreferrer">Jacky Chu</a></p>
-    </div>
+    <!-- <p>
+      <a href="https://www.instagram.com/hk_lax_walls/" class="social-icon" target="_blank" rel="noopener noreferrer">
+        <font-awesome-icon :icon="['fab', 'instagram']"/>
+      </a>
+    </p> -->
+    <p>HKLAXWALLS © 2020</p>
+    <p>Website by <a href="https://github.com/chujacky/" target="_blank" rel="noopener noreferrer">Jacky Chu</a></p>
   </footer>
 </template>
 
