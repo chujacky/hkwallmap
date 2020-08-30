c92754fd38e4172c71f02fab4a92ec3cc557fbf2
chujacky <jacky9407@gmail.com>
2020-08-31 00:52:30 +0800

    Add animations

--- components/MarkerItem.vue
+++ components/MarkerItem.vue
@@ -1,46 +1,53 @@
 <template>
-  <div class="marker-meta-wrapper box" v-if="Object.keys(marker).length">
-    <div class="marker-content">
-      <font-awesome-icon :icon="['fas', 'times']" @click="close"/>
-      <div class="marker-meta">
-    </div>
-      <div class="meta-item image-wraper" v-if="marker.imageUrl">
-        <figure class="image is-3by2">
-          <img :src="marker.imageUrl">
-        </figure>
+  <transition name="fade">
+    <div class="marker-meta-wrapper box" :key="isDesktop ? marker.id : 1">
+      <div class="marker-content">
+        <font-awesome-icon :icon="['fas', 'times']" @click="close"/>
+        <div class="marker-meta">
       </div>
-      <h2 class="title is-5 mb-4">{{marker.name}}</h2>
-      <div class="safety-notes meta-item">
-        <p class="has-text-weight-semibold mb-1">Trouble/Passby</p>
-        <p>{{marker.trouble}}</p>
-      </div>
-      <div class="time-notes meta-item">
-        <p class="has-text-weight-semibold mb-1">Timing</p>
-        <p>{{marker.time}}</p>
-      </div>
-      <div class="space-notes meta-item">
-        <p class="has-text-weight-semibold mb-1">Space</p>
-        <p>{{marker.space}}</p>
-      </div>
-      <div class="ball-safety-notes meta-item">
-        <p class="has-text-weight-semibold mb-1">Chance of losing ball</p>
-        <p>{{marker.ballSafety}}</p>
-      </div>
-      <div class="transportation-notes meta-item">
-        <p class="has-text-weight-semibold mb-1">Transportation</p>
-        <p>{{marker.transportation}}</p>
-      </div>
-    </div> 
+        <div class="meta-item image-wraper" v-if="marker.imageUrl">
+          <figure class="image is-3by2">
+            <img :src="marker.imageUrl">
+          </figure>
+        </div>
+        <h2 class="title is-5 mb-4">{{marker.name}}</h2>
+        <div class="safety-notes meta-item">
+          <p class="has-text-weight-semibold mb-1">Trouble/Passby</p>
+          <p>{{marker.trouble}}</p>
+        </div>
+        <div class="time-notes meta-item">
+          <p class="has-text-weight-semibold mb-1">Timing</p>
+          <p>{{marker.time}}</p>
+        </div>
+        <div class="space-notes meta-item">
+          <p class="has-text-weight-semibold mb-1">Space</p>
+          <p>{{marker.space}}</p>
+        </div>
+        <div class="ball-safety-notes meta-item">
+          <p class="has-text-weight-semibold mb-1">Chance of losing ball</p>
+          <p>{{marker.ballSafety}}</p>
+        </div>
+        <div class="transportation-notes meta-item">
+          <p class="has-text-weight-semibold mb-1">Transportation</p>
+          <p>{{marker.transportation}}</p>
+        </div>
+      </div> 
   </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'marker-item',
+  data() {
+    return {
+      isDesktop: true
+    }
+  },
   computed: {
     marker() {
       return this.$store.state.activeMarker;
-    }
+    },
   },
   methods: {
     open() {
@@ -52,8 +59,13 @@ export default {
   },
   mounted() {
     this.$root.$on('open', () => {
+      if (this.isDesktop) {
+        return;
+      }
       this.open();
     });
+
+    this.isDesktop = window.innerWidth > 768;
   },
 }
 </script>
