8b63aa7bb52896e41684feccdbe198b8bbcff8b0
chujacky <jacky9407@gmail.com>
2020-08-31 10:12:57 +0800

    Revert "Install vue touch for swiping action"
    This reverts commit edd8ad579370146adf6d01779446122363892a75.

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
-  plugins: ["@/plugins/google-map", '@/plugins/firebase', '@/plugins/vue-touch'],
+  plugins: ["@/plugins/google-map", '@/plugins/firebase'],
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
