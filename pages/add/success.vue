5cf550cd032cf35ff41cb36eb28d9dc2cf0351d2
chujacky <jacky9407@gmail.com>
2020-09-04 00:45:54 +0800

    Update success page desgin

--- pages/add/success.vue
+++ pages/add/success.vue
@@ -1,8 +1,11 @@
 <template>
   <section class="section success-section">
     <div class="container">
-      <font-awesome-icon :icon="['fas', 'check-circle']" />
-      <p class="thanks-text">Thank you for your submission!</p>
+      <!-- <font-awesome-icon :icon="['fas', 'check-circle']" /> -->
+      <div class="image-container">
+        <img src="/success-wall.png" alt="">
+      </div>
+      <h2 class="thanks-text title">Your wall has been successfully added!</h2>
 
       <div class="links-container">
         <nuxt-link to="/">Back to homepage</nuxt-link>
